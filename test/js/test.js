5a4c1f02ad73b39fd7dad29971284c44453aa2cb
KoenLee <koenmagic@hotmail.com>
2017-07-06 08:02:49 +0800

    Update test.js

--- test/js/test.js
+++ test/js/test.js
@@ -1,37 +1,41 @@
-	var page=1;
-	var progress=0;
+var page=1;
+var progress=0;
+	
+$('.page').css({
+		fontSize: $('.circle').height()*0.28,
+		lineHeight: $('.circle').height()+'px'
+});
 
-
-	//点击图片切换
-	$('.pic').click(function() {	
-		if (page<10) {
-			page++;
-			skip();
-		}	
-		//测试进度，累加到10的时候出结果
-		progress++;
-		if(progress==10){
-			//出结果.......
-		}
-	});
-
-	//点击退回键
-	$('#prev').click(function() {
-		if (page>=2) {
-			page--;
-			skip();
-			console.log(progress);
-		}
-	});
-
-	//切换当前进度和图片
-	function skip(){
-		$('.current-page').text(page);
-		$('#picA').animate({opacity:0.8},400,function(){
-				$('#picA').attr('src', 'img/'+page+'A.jpg').animate({opacity:1},400)
-			});
-
-		$('#picB').animate({opacity:0.8},400,function(){
-			$('#picB').attr('src', 'img/'+page+'B.jpg').animate({opacity:1},400)
-		});
+//点击图片切换
+$('.pic').click(function() {	
+	if (page<10) {
+		page++;
+		skip();
+	}	
+	//测试进度，累加到10的时候出结果
+	progress++;
+	if(progress==10){
+		//出结果.......
 	}
+});
+
+//点击退回键
+$('#prev').click(function() {
+	if (page>=2) {
+		page--;
+		skip();
+		console.log(progress);
+	}
+});
+
+//切换当前进度和图片
+function skip(){
+	$('.current-page').text(page);
+	$('#picA').animate({opacity:0.8},400,function(){
+			$('#picA').attr('src', 'img/'+page+'A.jpg').animate({opacity:1},400)
+		});
+
+	$('#picB').animate({opacity:0.8},400,function(){
+		$('#picB').attr('src', 'img/'+page+'B.jpg').animate({opacity:1},400)
+	});
+}
