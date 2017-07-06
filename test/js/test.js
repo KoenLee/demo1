da925a9f9dde401da1752267c648b7433f263e9c
KoenLee <koenmagic@hotmail.com>
2017-07-06 08:11:02 +0800

    Update test.js

--- test/js/test.js
+++ test/js/test.js
@@ -1,30 +1,5 @@
 var page=1;
 var progress=0;
-	
-//获取窗口宽度
-var W=$(window).width();
-
-//设置圆圈样式
-$('.circle').css({
-	width: W*0.125,
-	height: W*0.125,
-	marginTop: W*0.0625,
-	marginBottom: W*0.0625
-});
-
-//设置题号样式
-$('.page').css({
-	fontSize: $('.circle').height()*0.28,
-	lineHeight: $('.circle').height()+'px'
-});
-
-//设置图片样式
-$('.pic').css({
-	marginBottom: W*0.0625,
-	width:W*0.8,
-	height:'auto'
-});
-
 
 //点击图片切换
 $('.pic').click(function() {	
