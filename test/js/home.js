d42a23c595e4efcfae4ac99c019bb96032038c99
KoenLee <koenmagic@hotmail.com>
2017-07-07 09:37:50 +0800

    Update home.js

--- test/js/home.js
+++ test/js/home.js
@@ -9,80 +9,26 @@ if (isWeixinBrowser()) {
 	$('.blank').eq(0).remove();
 }
 
-//根据窗口尺寸调整其他元素尺寸和垂直居中设置
-var W=$(window).width();
-var hW=$('.header').width();
-
-$('.content').css({
-	padding: W*0.078,
-});
-if (W>500) {
-	$('.content').css('padding',25)
-}
-$('.article-text').css({
-	fontSize: W*0.0375,
-});
-$('.article-small').css({
-	fontSize: W*0.0343,
-});
-$('#result-title').css({
-	fontSize: W*0.04375
-});
-$('#article-title').css({
-	fontSize: W*0.04375
-});
-if (W>500) {
-	$('.article-text').css('fontSize', 20);
-	$('#result-title').css('fontSize', 24);
-	$('#article-title').css('fontSize', 24);
-	$('.article-small').css('fontSize', 16);
-};
-$('figcaption').css({
-	fontSize: W*0.03125,
-});
-if (W>500) {
-	$('figcaption').css('fontSize', 16);
-};
-$('img').css({
-	marginBottom:hW*0.0265
-})
-if (W>500){
-	$('img').css('marginBottom',15)
-}
-$('.button').css({
-	height: W*0.153,
-	fontSize:W*0.04375,
-	lineHeight: W*0.153+'px'
-});
-if (W>500) {
-	$('.button').css('lineHeight', '60px');
-	$('.button').css('fontSize', 20);
-};
-$('.blank').css({
-	height:$('#header').height()
-});
-
-for (var i = 0; i < $('.icon').length; i++) {
-	$('.icon').eq(i).css({
-		marginTop: $('.discription').eq(i).height()*0.5-$('.icon').eq(i).width()*0.5
-	});
-
-	if(parseInt($('.icon').eq(i).css('marginTop'))<=0){
-		$('.icon').eq(i).css('marginTop',0)
-	}
-}
+//文档载入时
+styleInitialize();
 
 //改变窗口大小（屏幕旋转）时
 $(window).resize(function() {
-	W=$(window).width();
-	hW=$('.header').width();
+	styleInitialize();
+})
+
+
+//根据窗口尺寸调整其他元素尺寸和居中设置
+function styleInitialize(){
+	var W=$(window).width();
+	var hW=$('.header').width();
 
 	$('.content').css({
 		padding: W*0.078,
 	});
 	if (W>500) {
 		$('.content').css('padding',25)
-	};
+	}
 	$('.article-text').css({
 		fontSize: W*0.0375,
 	});
@@ -112,11 +58,11 @@ $(window).resize(function() {
 	})
 	if (W>500){
 		$('img').css('marginBottom',15)
-	};
+	}
 	$('.button').css({
 		height: W*0.153,
 		fontSize:W*0.04375,
-		lineHeight:$('.button').height()+'px'
+		lineHeight: W*0.153+'px'
 	});
 	if (W>500) {
 		$('.button').css('lineHeight', '60px');
@@ -125,6 +71,7 @@ $(window).resize(function() {
 	$('.blank').css({
 		height:$('#header').height()
 	});
+
 	for (var i = 0; i < $('.icon').length; i++) {
 		$('.icon').eq(i).css({
 			marginTop: $('.discription').eq(i).height()*0.5-$('.icon').eq(i).width()*0.5
@@ -134,7 +81,4 @@ $(window).resize(function() {
 			$('.icon').eq(i).css('marginTop',0)
 		}
 	}
-
-
-
-})
+}
