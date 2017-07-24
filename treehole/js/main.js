f7ac8a955b15e9d596b4cbeba617d8271577c74d
KoenLee <koenmagic@hotmail.com>
2017-07-24 20:02:04 +0800

    Update main.js

--- treehole/js/main.js
+++ treehole/js/main.js
@@ -10,6 +10,7 @@ function styleInitialize(){
 	$('.brand').css('height', $('.brand').width()*0.448);
 	$('.text-normal').css('fontSize', W*0.034375);
 	$('.number').css('margin-bottom', W*0.0234);
+	$('#danmu').css('height', W*0.4375);
 }
 
 
