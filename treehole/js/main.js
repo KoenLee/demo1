3ab83c739e379a38fe9f9aad2e8f433e1f1ed663
KoenLee <koenmagic@hotmail.com>
2017-07-27 15:30:55 +0800

    Update main.js

--- treehole/js/main.js
+++ treehole/js/main.js
@@ -2,6 +2,7 @@ $(function(){
 	styleInitialize();
 	dotAnimation();
 	document.ondragstart=function() {return false;};
+	var window=window[0];
 	console.log($(window));
 	console.log(window);
 	console.log(window.scrollY);
