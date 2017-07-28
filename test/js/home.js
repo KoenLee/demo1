5dbec5e76d93e9c5f41b2aceb6a1ffebc31f4d61
KoenLee <koenmagic@hotmail.com>
2017-07-28 13:52:19 +0800

    Update home.js

--- test/js/home.js
+++ test/js/home.js
@@ -12,14 +12,11 @@ if (isWeixinBrowser()) {
 }
 
 $(function(){  
-          //  pushHistory();  
+          pushHistory();  
             window.addEventListener("popstate", function(e) {  
-              	 goBack(2)
-            	//alert("我监听到了浏览器的返回按钮事件啦");//根据自己的需求实现自己的功能  
-               //	window.location=home.html;
-               // pushHistory();  
-		
-                  
+         	pushHistory();
+            	//alert("我监听到了浏览器的返回按钮事件啦");//根据自己的需求实现自己的功能
+		    window.history.back()
         }, false);  
         });
 
