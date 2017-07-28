016fdae845d803bdcc67983bd4917ba72965b4ea
KoenLee <koenmagic@hotmail.com>
2017-07-28 14:05:12 +0800

    Update home.js

--- test/js/home.js
+++ test/js/home.js
@@ -16,7 +16,8 @@ $(function(){
             window.addEventListener("popstate", function(e) {  
          	pushHistory(); 
             	//alert("我监听到了浏览器的返回按钮事件啦");//根据自己的需求实现自己的功能
-		    window.history.go(-1)
+		    window.history.go(-1);
+		    retutn false;
         }, false);  
         });
 
